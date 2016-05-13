f3ba74c2649068cd4c06c32e15428d9d9b956efd
cmw72d <cmw72d@mail.missouri.edu>
2016-05-12 22:44:10 -0500

    Update worldseriesandmizzou.sql

--- worldseriesandmizzou.sql
+++ worldseriesandmizzou.sql
@@ -2,18 +2,22 @@ SELECT * FROM tweet WHERE job_id = 2557 LIMIT 50;
 
 SELECT * FROM tweet WHERE job_id = 4257 LIMIT 50;
 
+/* Answer to question 1 */
 SELECT COUNT(*) FROM tweet WHERE job_id = 2577;
 
 SELECT COUNT(*) FROM tweet WHERE job_id = 4257;
 
-SELECT MAX(created_at) FROM tweet WHERE job_id = 2577;
-
-SELECT MAX(created_at) FROM tweet WHERE job_id = 4257;
-
+/* Answer to question 2 */ 
 SELECT MIN(created_at)	FROM tweet 	WHERE job_id = 2577;
 
 SELECT MIN(created_at)	FROM tweet 	WHERE job_id = 4257;
 
+/* Answer to question 3 */
+SELECT MAX(created_at) FROM tweet WHERE job_id = 2577;
+
+SELECT MAX(created_at) FROM tweet WHERE job_id = 4257;
+
+/* What I used to plot in my R file */
 SELECT MONTH(created_at) AS theMonth, WEEK(created_at) AS theWeek, DAY(created_at) AS theDay, YEAR(created_at) AS theYear, COUNT(*) AS counter
 FROM tweet WHERE job_id = 2577 
 GROUP BY theYear, theMonth, theWeek, theDay;
